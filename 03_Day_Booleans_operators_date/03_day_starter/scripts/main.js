bb7a3fcf02e0b0b6b5696bb1a097f3a783b77149
latanam <latanam@wwu.edu>
2022-03-06 14:56:23 -0800

    Answered some questions day 3

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,67 @@
 // this is your main.js script
+/*
+ * 1. Declare firstName, lastName, country, city, age, isMarries, year
+ * variables and assign value to it and use the type of operator
+ * to check different data types.
+ */
+console.log('Question 1:')
+
+let firstName = 'Matthew'
+let lastName = 'Latanafrancia'
+let country = 'United States'
+let city = 'Bellingham'
+let age = 19
+let isMarried = false
+let year = 2022
+
+console.log(`firstName: ${firstName} ${typeof firstName}`)
+console.log(`lastName: ${lastName} ${typeof lastName}`)
+console.log(`country: ${country} ${typeof country}`)
+console.log(`city: ${city} ${typeof city}`)
+console.log(`age: ${age} ${typeof age}`)
+console.log(`isMarried: ${isMarried} ${typeof isMarried}`)
+console.log(`year: ${year} ${typeof year}`)
+
+/*
+ * 2. Check if type of '10' is equal to 10
+ */
+console.log(`Question 2:`)
+console.log(`The answer to question 2 is: ${typeof '10' == typeof 10}`)
+
+/*
+ *3. Check if parseInt('9.8') is equal to 10
+ */
+console.log(`Question 3:`)
+console.log(`The answer to questions 3 is: ${parseInt('9.8') == 10}`)
+
+/*
+ * 4. Boolean value is either true or false.
+ *      1. Write three JavaScript statement which provide truthy value.
+ *      2. Write three JavaScript statement which provide falsy value.
+ */
+console.log(`Question 4`)
+
+/*
+ * 5. Figure out the result of the following omparison expression first
+ * without using console.log().   After you decide the result confirm it using
+ * console.log
+ */
+console.log(`Question 5`)
+
+/*
+ * 6. Figure out the result of the following expressions first without using
+ * console.log().  After you decide the result confirm it by using console.log()
+ */
+console.log(`Questions 6`)
+
+/*
+ * 7. Use the Date object to do the following activities
+ *      1. What is the year today?
+ *      2. What is the month today as a number?
+ *      3.  What is the date today?
+ *      4. What is the day today as a number?
+ *      5. What is the hours now?
+ *      6. What is the minutes now?
+ *      7. Find out the number of seconds elapsed from January 1, 1970 to now.
+ */
+console.log(`Question 7`)
