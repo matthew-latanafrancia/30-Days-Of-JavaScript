c26d1da0bc5d15d19a94ec6855788a0eaddf0626
latanam <latanam@wwu.edu>
2022-03-28 02:50:56 -0700

    Started level 2 day 3 exercises

--- 02_Day_Data_types/day2/30DaysOfJS_Day2Level2.js
+++ 02_Day_Data_types/day2/30DaysOfJS_Day2Level2.js
@@ -46,6 +46,3 @@ console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125')
 let fullString = 'You cannot end a sentence with because because because is a conjuction.'
 let subString = fullString.substr(31, 23)
 console.log(subString)
-
-
-
---
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/level2.js
@@ -0,0 +1,111 @@
+console.log(`Exercises: Level 2`)
+
+/*
+ * 1. Write a script that prompt the user to enter base and height
+ * of the triangle and calculate an area of a triangle.
+ */
+
+console.log(`Question 1`)
+
+let base = prompt("Enter the base of the trianle")
+let height = prompt("Enter height of the trianle")
+
+let area = base*height*0.5
+
+console.log(`The area of the inputted triangle is ${area}`)
+
+/*
+ * 2. Write a script that prompt the user to enter side a, side b,
+ * and side c of the triangle and calculate the perimeter of triangle
+ */
+console.log(`Question 2`)
+
+let a = parseInt(prompt("Enter the length of side 1"))
+let b = parseInt(prompt("Enter the length of side 2"))
+let c = parseInt(prompt("Enter the length of side 3"))
+
+let pertri = a+b+c
+
+console.log(`The perimeter of the triangle is ${pertri}`)
+
+/*
+ * 3. Get length and width using prompt and calculate an area of rectangle
+ * and width of the rectangle
+ */
+console.log(`Question 3`)
+
+let len = parseInt(prompt("Enter the length of the rectangle."))
+let wid = parseInt(prompt("Enter the width of the rectangle"))
+
+let are = len*wid
+let per = (len+wid)*2
+
+console.log(`The area of the rectangle is ${are} and the perimeter is ${per}`)
+
+/*
+ * 4. Get radius usiong prompt and calculate the area of a circle
+ */
+
+console.log(`Question 4`)
+
+let radius = parseInt(prompt('Enter the radius of the circle'))
+
+console.log(`The area of the circle is ${Math.PI * radius * radius}`)
+
+/*
+ * 5. Calculate the slope, x-intercept and y-intercept of y=2x-2
+ */
+
+/*
+ * 6. Slope is (y_2-y_1)/(x_2-x_1). Find the slope between point (2,2)
+ * and point (6,10)
+ */
+
+/*
+ * 7. Compare the slope of above two questions.
+ */
+
+/*
+ * 8. Calculate the value of y (y = x^2 + 6x + 9).  Try to use
+ * different x values and figure out at what x value y is 0
+ */
+
+/*
+ * 9. Write a script that prompt a user to enter hours and rate
+ * per hour.  Calculate pay of the person?
+ */
+
+/*
+ * 10. If the length of your name is greater than 7 say, your name is long
+ * else say your name is short
+ */
+
+/*
+ * 11. Compare your first name length and your family name length and you should
+ * get this output
+ */
+
+/*
+ * 12. Declare two variables myAge and yourAge and assign them
+ * initial values and myAge and yourAge
+ */
+
+
+/*
+ * 13. Using prompt get the year the user was born and if the user is 18
+ * or above allow the user to drive if not tell the user to wait a certain
+ * amount of years.  Driving is when you turn 18
+ */
+
+/*
+ * 14. Write a cript that prompt the user to enter number of years.  Calculate
+ * the number of seconds a person can live.  Assume some one lives just hundred years.
+ */
+
+/*
+ * 15. Create a human readable time format using the Date time object.
+ */
+
+
+
+
