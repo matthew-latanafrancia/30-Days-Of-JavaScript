02834bdba1e3f27b28b7d150b3f1c3e933a16476
latanam <latanam@wwu.edu>
2022-04-03 19:41:47 -0700

    Day 5 Excercises

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,53 @@
 console.log(countries)
 alert('Open the browser console whenever you work on JavaScript')
 alert('Open the console and check if the countries has been loaded')
+
+// 1.
+const emptyArr = []
+const emptyObj = Array()
+
+/*
+Declare an array with more than 5 number of elements
+Find the length of your array
+Get the first item, the middle item and the last item of the array
+Declare an array called mixedDataTypes, put different data types in the array and find the length of the array. The array size should be greater than 5
+Declare an array variable name itCompanies and assign initial values Facebook, Google, Microsoft, Apple, IBM, Oracle and Amazon
+Print the array using console.log()
+Print the number of companies in the array
+Print the first company, middle and last company
+Print out each company
+Change each company name to uppercase one by one and print them out
+Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
+Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+Filter out companies which have more than one 'o' without the filter method
+Sort the array using sort() method
+Reverse the array using reverse() method
+Slice out the first 3 companies from the array
+Slice out the last 3 companies from the array
+Slice out the middle IT company or companies from the array
+Remove the first IT company from the array
+Remove the middle IT company or companies from the array
+Remove the last IT company from the array
+Remove all IT companies
+*/
+
+const fiveArr = [1, 2, 3, 4, 5]
+
+console.log(`The length of the five array is ${fiveArr.length}`)
+
+console.log(`First item: ${fiveArr[0]}`)
+console.log(`Middle item: ${fiveArr[fiveArr.length/2]}`)
+console.log(`Last item: ${fiveArr[fiveArr.length-1]}`)
+
+const mixedDataTypes = ["hello", 1, 4, true, null]
+const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+
+console.log(itCompanies.toString())
+console.log(itCompanies.length)
+
+//print the middle things and the end pieces and stuff
+
+
+
+
+
