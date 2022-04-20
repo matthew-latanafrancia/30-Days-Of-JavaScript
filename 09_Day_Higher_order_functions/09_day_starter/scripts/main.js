65c5b7ea0cb61a2e0be766f3615c1618ee0ef602
Matthew Latanafrancia <latanam@cf164-24.cs.wwu.edu>
2022-04-19 19:18:42 -0700

    Started day 9 exercises

--- 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
+++ 09_Day_Higher_order_functions/09_day_starter/scripts/main.js
@@ -1,2 +1,55 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+/*
+1. Explain the difference between **_forEach, map, filter, and reduce_**.
+2. Define a callback function before you use it in forEach, map, filter or reduce.
+3. Use **_forEach_** to console.log each country in the countries array.
+4. Use **_forEach_** to console.log each name in the names array.
+5. Use **_forEach_** to console.log each number in the numbers array.
+6. Use **_map_** to create a new array by changing each country to uppercase in the countries array.
+7. Use **_map_** to create an array of countries length from countries array.
+8. Use **_map_** to create a new array by changing each number to square in the numbers array
+9. Use **_map_** to change to each name to uppercase in the names array
+10. Use **_map_** to map the products array to its corresponding prices.
+11. Use **_filter_** to filter out countries containing **_land_**.
+12. Use **_filter_** to filter out countries having six character.
+13. Use **_filter_** to filter out countries containing six letters and more in the country array.
+14. Use **_filter_** to filter out country start with 'E';
+15. Use **_filter_** to filter out only prices with values.
+16. Declare a function called getStringLists which takes an array as a parameter and then returns an array only with string items.
+17. Use **_reduce_** to sum all the numbers in the numbers array.
+18. Use **_reduce_** to concatenate all the countries and to produce this sentence: **_Estonia, Finland, Sweden, Denmark, Norway, and IceLand are north European countries_**
+19. Explain the difference between **_some_** and **_every_**
+20. Use **_some_** to check if some names' length greater than seven in names array
+21. Use **_every_** to check if all the countries contain the word land
+22. Explain the difference between **_find_** and **_findIndex_**.
+23. Use **_find_** to find the first country containing only six letters in the countries array
+24. Use **_findIndex_** to find the position of the first country containing only six letters in the countries array
+25. Use **_findIndex_** to find the position of **_Norway_** if it doesn't exist in the array you will get -1.
+26. Use **_findIndex_** to find the position of **_Russia_** if it doesn't exist in the array you will get -1.
+*/
+//1
+/*
+What forEach will do is run through the higher order function for each of
+the elements in the array. Map will iterate an arrays elements and modifies them.
+In other words, map will map the values to new values depending on the values
+in the element and returns an array.  Filer will filter out items that meet a condition
+where its truthy.  Reduce will reduce the array into a single value
+*/
+
+//2
+countries.forEach(function (elements, index, arr) {
+    console.log(arr[index].name)
+})
+
+
+//3
+//this is like the same thing as the last problem so im not gonna do it
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.forEach(function(num) {
+    console.log(num)
+})
+
+//4 
+
+
+
