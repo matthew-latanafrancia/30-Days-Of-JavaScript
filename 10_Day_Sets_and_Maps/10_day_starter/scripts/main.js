5d784eb9ead33163acbe5dc6de2d16077be0a5e9
latanam <latanam@wwu.edu>
2022-05-08 00:19:19 -0700

    Finished day 10 level 2

--- 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
+++ 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
@@ -37,3 +37,22 @@ countryMap.set(yamato, yamato.length)
 countryMap.set(america, america.length)
 
 console.log(countryMap)
+
+//Exercises level 2
+//1. find a union b
+const a = [1,2,3,4,5]
+const b = [3,4,5,6,7,8]
+const c = [...a, ...b]
+
+const aSet = new Set(a)
+const bSet = new Set(b)
+const cSet = new Set(c)
+console.log(cSet)
+//2. Find a intersecion b
+const cIntersect = a.filter((num) => bSet.has(num))
+//3. Find a difference with b
+const cDifference = a.filter((num) => !bSet.has(num))
+
+console.log(cSet)
+console.log(cIntersect)
+console.log(cDifference)
