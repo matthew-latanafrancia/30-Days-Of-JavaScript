2c23844ddf1ec25bcaefa48bd0c104e27c7d6038
Matthew Latanafrancia <latanam@cf418-05.cs.wwu.edu>
2022-05-11 03:10:56 -0700

    Started Day 12 Exercises

--- 12_Day_Regular_expressions/12_day_starter/scripts/main.js
+++ 12_Day_Regular_expressions/12_day_starter/scripts/main.js
@@ -1,2 +1,24 @@
 console.log(countries)
 alert('Open the console and check if the countries has been loaded')
+
+// 1. Calculate the total annual income of the person from the following text. 
+//‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
+
+// 2. The position of some particles on the horizontal x-axis -12, -4, -3 and  -1 in the negative direction, 0 at origin, 
+//4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
+
+// ```js
+// points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
+// sortedPoints =  [-4, -3, -1, -1, 0, 2, 4, 8]
+// distance = 12
+// ```
+
+// 3. Write a pattern which identify if a string is a valid JavaScript variable
+
+//     ```sh
+//     is_valid_variable('first_name') # True
+//     is_valid_variable('first-name') # False
+//     is_valid_variable('1first_name') # False
+//     is_valid_variable('firstname') # True
+//     ```
+
