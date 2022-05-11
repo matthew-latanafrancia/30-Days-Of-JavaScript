fa1fda4e155cdae814de97a9696992cbd58a861e
Matthew Latanafrancia <latanam@cf418-05.cs.wwu.edu>
2022-05-11 03:17:46 -0700

    Finished day 13 level 1 exercises

--- 13_Day_Console_object_methods/13_day_starter/scripts/main.js
+++ 13_Day_Console_object_methods/13_day_starter/scripts/main.js
@@ -1,2 +1,14 @@
 console.log(countries)
 alert('Open the console and check if the countries has been loaded')
+
+//1.
+
+console.table(countries)
+
+//2
+console.log(countries)
+
+//3
+console.group('Names')
+console.log(names)
+console.groupEnd()
