4b3000dad193bb673254a72a5b11b3c4b5aee487
Matthew Latanafrancia <latanam@cf418-05.cs.wwu.edu>
2022-05-17 01:33:03 -0700

    started day 21 exercises

---
+++ 21_Day_DOM/21_day_starter/scripts/main.js
@@ -0,0 +1,11 @@
+// Create an index.html file and put four p elements as above: Get the first paragraph by using document.querySelector(tagname) and tag name
+const testArr = document.querySelector("p");
+console.log(testArr[8]);
+
+
+
+// Get get each of the the paragraph using document.querySelector('#id') and by their id
+// Get all the p as nodeList using document.querySelectorAll(tagname) and by their tag name
+// Loop through the nodeList and get the text content of each paragraph
+// Set a text content to paragraph the fourth paragraph,Fourth Paragraph
+// Set id and class attribute for all the paragraphs using different attribute setting methods
